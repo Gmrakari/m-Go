2019cd19dc41085789f65d477eba7d9f8f040a02
Gmrakari <gmrakari@outlook.com>
2022-05-24 16:20:14 +0800

    [add]append slices

---
+++ 03-Slice 切片/demo_3.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	sli := [] int {4, 5, 6}
+	fmt.Printf("len = %d cap = %d slice = %v\n",len(sli), cap(sli), sli)
+
+	sli = append(sli, 7)
+	fmt.Printf("len = %d cap = %d slice = %v\n",len(sli), cap(sli), sli)
+
+	sli = append(sli, 8)
+	fmt.Printf("len = %d cap = %d slice = %v\n",len(sli), cap(sli), sli)
+
+	sli = append(sli, 9)
+	fmt.Printf("len = %d cap = %d slice = %v\n",len(sli), cap(sli), sli)
+}
